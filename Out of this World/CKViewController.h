bb2f375dbeccfa8a708b0667f199a954c5f3ec7a
Devin Medeiros <devin_medeiros@Devin-Medeiross-MacBook-Pro.local>
2014-04-08 15:50:53 -1000

    Initial Commit

---
+++ Out of this World/CKViewController.h
@@ -0,0 +1,13 @@
+//
+//  CKViewController.h
+//  Out of this World
+//
+//  Created by Devin Medeiros on 4/8/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKViewController : UIViewController
+
+@end
---
+++ Out of this World/CKViewController.m
@@ -0,0 +1,29 @@
+//
+//  CKViewController.m
+//  Out of this World
+//
+//  Created by Devin Medeiros on 4/8/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import "CKViewController.h"
+
+@interface CKViewController ()
+
+@end
+
+@implementation CKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
